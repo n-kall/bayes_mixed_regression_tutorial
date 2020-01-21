3a737f1fd8218e5e64384ac861b6e1d42e4fce1f
n-kall <n-kall@github.com>
2020-01-21 20:19:57 +1000

    add hypothesis functionality

--- faintr/R/extra.R
+++ faintr/R/extra.R
@@ -1,170 +1,11 @@
 library(brms)
 library(tidyverse)
 
-all_terms <- function(x) {
-  if (!length(x)) {
-    return(character(0))
-  }
-  if (!inherits(x, "terms")) {
-    x <- terms(as.formula(x))
-  }
-  rm_wsp(attr(x, "term.labels"))
-}
-
-get_effect.brmsterms <- function(x, target = "fe", all = TRUE, ...) {
-  if (all) {
-    out <- named_list(c(names(x$dpars), names(x$nlpars)))
-    for (dp in names(x$dpars)) {
-      out[[dp]] <- get_effect(x$dpars[[dp]], target = target)
-    }
-    for (nlp in names(x$nlpars)) {
-      out[[nlp]] <- get_effect(x$nlpars[[nlp]], target = target)
-    }
-  } else {
-    out <- get_effect(x$dpars[["mu"]], target = target)
-  }
-  unlist(out, recursive = FALSE)
-}
-
-# extract different types of effects
-get_effect <- function(x, ...) {
-  UseMethod("get_effect")
-}
-
-
-# extract names of variables treated as integers
-get_int_vars <- function(x, ...) {
-  UseMethod("get_int_vars")
-}
-
-#' @export
-get_int_vars.brmsterms <- function(x, ...) {
-  advars <- ulapply(rmNULL(x$adforms[c("trials", "cat")]), all_vars)
-  unique(c(advars, get_sp_vars(x, "mo")))
-}
-
-
-# unlist lapply output
-ulapply <- function(X, FUN, ..., recursive = TRUE, use.names = TRUE) {
-  unlist(lapply(X, FUN, ...), recursive, use.names)
-}
-
-
-# check if an object is NULL
-isNULL <- function(x) {
-  is.null(x) || ifelse(is.vector(x), all(sapply(x, is.null)), FALSE)
-}
-
-# recursively removes NULL entries from an object
-rmNULL <- function(x, recursive = TRUE) {
-  x <- Filter(Negate(isNULL), x)
-  if (recursive) {
-    x <- lapply(x, function(x) if (is.list(x)) rmNULL(x) else x)
-  }
-  x
-}
-
-
-# find variables in a character string or expression
-all_vars <- function(expr, ...) {
-  if (is.character(expr)) {
-    expr <- parse(text = expr)
-  }
-  all.vars(expr, ...)
-}
-
-# find names of all variables used in a special effects type
-get_sp_vars <- function(x, type) {
-  sp_terms <- ulapply(get_effect(x, "sp"), all_terms)
-  all_vars(str2formula(get_matches_expr(regex_sp(type), sp_terms)))
-}
-
-# initialize a named list
-# @param names names of the elements
-# @param values optional values of the elements
-named_list <- function(names, values = NULL) {
-  if (!is.null(values)) {
-    if (length(values) <= 1L) {
-      values <- replicate(length(names), values)
-    }
-    values <- as.list(values)
-    stopifnot(length(values) == length(names))
-  } else {
-    values <- vector("list", length(names))
-  }
-  setNames(values, names)
-}
-
-#' @export
-get_effect.btl <- function(x, target = "fe", ...) {
-  x[[target]]
-}
-
-
-
-# convert a string to a formula
-# @param x vector of strings to be converted
-# @param ... passed to formula()
-str2formula <- function(x, ..., collapse = "+") {
-  has_chars <- nzchar(x)
-  if (length(x) && any(has_chars)) {
-    out <- paste(x[has_chars], collapse = collapse) 
-  } else {
-    out <- "1"
-  }
-  out <- formula(paste("~", out), ...)
-  environment(out) <- parent.frame()
-  out
-}
-
-# find matches in the parse tree of an expression
-# @param pattern pattern to be matched
-# @param expr expression to be searched in
-# @return character vector containing matches
-get_matches_expr <- function(pattern, expr, ...) {
-  if (is.character(expr)) {
-    expr <- parse(text = expr)
-  }
-  out <- NULL
-  for (i in seq_along(expr)) {
-    sexpr <- try(expr[[i]], silent = TRUE)
-    if (!is(sexpr, "try-error")) {
-      sexpr_char <- deparse_combine(sexpr)
-      out <- c(out, get_matches(pattern, sexpr_char, ...))
-    }
-    if (is.call(sexpr) || is.expression(sexpr)) {
-      out <- c(out, get_matches_expr(pattern, sexpr, ...))
-    }
-  }
-  unique(out)
-}
-
-
-
-form <- brms::bf(depvar ~ invar1 * invar2, family = "gaussian")
-
-parsed <- brms::parse_bf(form)
-
-
-
-
-
-
- dependent_variable <- as.character(form[[1]])[[2]]
-independent_variables  <- strsplit(x = gsub(pattern = "\\(.*\\|.*\\)",
-                                            "",
-                                            as.character(form[[1]])[[3]]),
-                                   split =  "(\\*|\\+)",
-                                   fixed = FALSE)[[1]] %>% trimws() %>% unique()
-  independent_variables = independent_variables[which(independent_variables != "")]
-
-
-
 iris_tibble  <- iris %>%
     mutate(Species = factor(Species),
            Sepal.Length = ordered(Sepal.Length))
 
-test_model <- brm(Sepal.Width ~ Species + mo(Sepal.Length), data = iris_tibble)
+test_model <- brm(Sepal.Width ~ Species + Sepal.Length, data = iris_tibble)
 
 
 get_variables <- function(model) {
@@ -185,7 +26,7 @@ get_variables <- function(model) {
                 predictors = predictors))
 }
 
-get_factor_predictors <- function(model) {
+get_factor_information <- function(model) {
     # return independent variables that are factors and their levels
 
     variables <- get_variables(model)
@@ -202,10 +43,61 @@ get_factor_predictors <- function(model) {
         names()
 
     # output levels for each of the predictor factors
-    factor_levels <- list()
+    factor_info <- list()
     for (fac in factor_predictors) {
-        factor_levels[[fac]] <- levels(data[[fac]])
+        lvls <- levels(data[[fac]])
+        ref_lvl <- lvls[1]
+        factor_levels <- list(levels = lvls,
+                              reference = ref_lvl)
+        
+        factor_info[[fac]] <- factor_levels
     }
 
-    return(factor_levels)
+    return(factor_info)
+}
+
+compare_cells <- function(model, higher, lower, alpha = 0.05) {
+
+    factor_info <- get_factor_information(model)
+
+    higher_str <- ""
+
+    higher_intercept = FALSE
+    
+    for (fct in names(higher)) {
+                                        # check for reference level
+        
+        if (factor_info[[fct]]$reference == higher[[fct]]) {
+            higher_intercept = TRUE
+            factor_str <- ""
+        } else {
+            higher_intercept = FALSE
+            factor_str <- paste0(fct, higher[[fct]])
+            }
+
+        if (higher_intercept == TRUE) {
+            higher_str <- "Intercept"
+        } else {
+            
+            if (length(higher) > 1) {
+                higher_str <- paste(higher_str, factor_str, sep = ":")
+            } else {
+                higher_str <- paste0(higher_str, factor_str)
+            }       
+        }
+    }
+
+    lower_str <- ""
+    for (fct in names(higher)) {
+        factor_str <- paste0(fct, lower[[fct]])
+        if (length(lower) > 1) {
+            lower_str <- paste(lower_str, factor_str, sep = ":")
+        } else {
+            lower_str <- paste0(lower_str, factor_str)
+        }
+    }
+
+
+    print(paste(higher_str, ">", lower_str))
+    brms::hypothesis(model, paste(higher_str, ">", lower_str), alpha = alpha)
 }
