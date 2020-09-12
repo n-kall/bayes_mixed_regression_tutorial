491076a406c0be6678c82b5d08981088deeac119
n-kall <n-kall@github.com>
2020-09-12 03:21:54 +0300

    fix model variable name, update vignette

--- doc/faintr_basics.R
+++ doc/faintr_basics.R
@@ -1,56 +1,63 @@
-## ----setup, include=FALSE, echo = FALSE, message = FALSE-----------------
-knitr::opts_chunk$set(echo=TRUE, 
-                      warning=FALSE,
-                      message=FALSE, 
+## ----setup, include=FALSE, echo = FALSE, message = FALSE----------------------
+knitr::opts_chunk$set(echo = TRUE,
+                      warning = FALSE,
+                      message = FALSE,
                       collapse = TRUE,
                       cache = TRUE,
-                      dev.args = list(bg = 'transparent'), 
-                      fig.align='center', 
-                      fig.height = 3, 
-                      fig.widht=4)
+                      dev.args = list(bg = "transparent"),
+                      fig.align = "center",
+                      fig.height = 3,
+                      fig.widht = 4)
 library(tidyverse)
 theme_set(theme_bw() + theme(plot.background=element_blank()) )
 
-## ---- eval = F-----------------------------------------------------------
-#  devtools::install_github('michael-franke/bayes_mixed_regression_tutorial/faintr',
-#                           build_vignettes = TRUE)
+## ---- eval = F----------------------------------------------------------------
+#  devtools::install_github(
+#    "n-kall/faintr",
+#    build_vignettes = TRUE
+#  )
 #  library(faintr)
 
-## ---- echo = F-----------------------------------------------------------
+## ---- echo = F----------------------------------------------------------------
 library(faintr)
 
-## ---- error=FALSE, warning=FALSE, message=FALSE--------------------------
+## ---- error=FALSE, warning=FALSE, message=FALSE-------------------------------
 library(tidyverse)
-politedata = read_csv('https://raw.githubusercontent.com/michael-franke/bayes_mixed_regression_tutorial/master/code/politeness_data.csv') 
-head(politedata)
+data(politeness)
 
-## ------------------------------------------------------------------------
-politedata %>% group_by(gender, context) %>% summarize(mean_pitch = mean(pitch))
+## -----------------------------------------------------------------------------
+politeness %>%
+    group_by(gender, context) %>%
+    summarize(mean_pitch = mean(pitch))
 
-## ---- error=FALSE, warning=FALSE, message=FALSE, results="hide"----------
+## ---- error=FALSE, warning=FALSE, message=FALSE, results="hide"---------------
 library(brms)
-m_dummy = brm(pitch ~ gender * context + (1 | subject + sentence), politedata)
+m_dummy <- brm(pitch ~ gender * context + (1 | subject + sentence), politeness)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 fixef(m_dummy)
 
-## ------------------------------------------------------------------------
-compare_groups(
-  model = m_dummy, 
-  higher = list(gender = "F", context = "pol"),
-  lower = list(gender = "M", context = "inf")
-)
+## -----------------------------------------------------------------------------
+female_polite <- faintr::filter_draws(m_dummy, gender == "F", context == "pol")
+male_informal <- faintr::filter_draws(m_dummy, gender == "M", context == "inf")
 
-## ------------------------------------------------------------------------
-compare_groups(
-  model = m_dummy, 
-  higher = list(gender = "F"),
-  lower = list()
-)
+## -----------------------------------------------------------------------------
+library(ggplot2)
 
-## ------------------------------------------------------------------------
-extract_posterior_cell_means(m_dummy)$all_cells_compared
+diff <- pull(female_polite) - pull(male_informal)
 
-## ------------------------------------------------------------------------
-extract_posterior_cell_means(m_dummy)$cell_summary
+mean(diff)
+
+qplot(diff, geom = "density")
+
+
+## -----------------------------------------------------------------------------
+
+library(ggplot2)
+
+bind_cols(female_polite, male_informal) %>%
+  gather(key = "cell") %>%
+  ggplot(aes(x = value, color = cell, fill = cell)) +
+  geom_density(alpha = 0.5)
+
 
